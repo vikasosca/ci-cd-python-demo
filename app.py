15a762f67a7c5ea14412324139ba735f29c88925
vikasosca <vikas.osca@gmail.com>
2025-07-21 13:06:58 +0530

    Fix YAML indentation for deploy step

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask is running!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
