21514205971e78235892d365148c7275147741cb
vikasosca <vikas.osca@gmail.com>
2025-07-21 12:39:00 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from CI/CD!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def add(a, b):
+    print(f"a is{a} and b is {b}")
+    return a + b
