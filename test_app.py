d7a0d3f00311e72145a19d68444ba37afadc61cf
vikasosca <vikas.osca@gmail.com>
2025-07-28 11:18:58 +0530

    Initial commit: calculator app

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"CI/CD running using flask" in response.data  # or your actual message
+    assert b"Hello, Flask is running!"  in response.data  # or your actual message
