a4a7579d8020e38bd39379671a3762719b2335f3
vikasosca <vikas.osca@gmail.com>
2025-08-04 11:15:15 +0530

    Added a new feature section to README

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 # Define a simple route
 @app.route("/")
 def home():
-    return "<h1>Hello from Flask!</h1><p>Welcome to CI/CD learning.</p>"
+    return "<h1>Hello from Flask App!</h1><p>Welcome to CI/CD learning.</p>"
 
 @app.route("/add/<int:a>/<int:b>")
 def add(a, b):
-    result = a + b
-    return f"<p>{a} + {b} = {result}</p>"
+    result = a * b
+    return f"<p>{a} * {b} = {result}</p>"
 
 # This makes it runnable locally
 if __name__ == "__main__":
